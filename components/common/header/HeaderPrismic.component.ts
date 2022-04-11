2c8be7306452a298c0084837298f9889a2eb6982
Yannick HOUSSIN <yannick.houssin@kissthebride.fr>
2022-04-11 09:36:02 +0200

    improving responsive

--- components/common/header/HeaderPrismic.component.ts
+++ components/common/header/HeaderPrismic.component.ts
@@ -2,4 +2,14 @@ import { Component } from 'nuxt-property-decorator';
 import Vue from 'vue';
 
 @Component
-export default class HeaderPrismicComponent extends Vue {}
+export default class HeaderPrismicComponent extends Vue {
+  mobileMenuOpen = false;
+
+  openMenu() {
+    this.mobileMenuOpen = true;
+  }
+
+  closeMenu() {
+    this.mobileMenuOpen = false;
+  }
+}
